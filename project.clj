5cadba684b3ba9746b1e32fb5058330502605f06
oylenshpeegul <theaney@gmail.com>
2017-08-22 18:20:13 -0400

    grabbed project.clj from Azel4321 to do lein run

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject azel4231.clojure-quotes "0.1.0-SNAPSHOT"
-  :description "Memorable quotes about Clojure "
+  :description "My favorite Clojure quotes"
   :url "https://github.com/Azel4231/clojure-quotes"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]]
+  :main clojure-quotes.core
+  :source-paths ["src"])
